6627f70b146fb2ab4af58af22ecee5a2a21bbf50
Joy Borja <joy@example.com>
2026-05-15 13:24:34 +0800

    Update API URL to Localtunnel and sync dependencies

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://192.168.43.166:8000/api";
+export const API_URL = "https://hot-readers-poke.loca.lt/api";
 
 //ALISDI ANG IP ADDRESS OG PORT KUNG KINSA NAG UP SA BACKEND SERVER

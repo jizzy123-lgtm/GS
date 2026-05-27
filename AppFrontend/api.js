a87cfc1f53b0b9eea0ed4f36021ba639fee1a6ad
userAlet22 <2mclaurence2005@gmail.com>
2026-05-27 17:52:35 +0800

    Update AppFrontend branding and deployed API URL

--- AppFrontend/api.js
+++ AppFrontend/api.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://100.82.99.76:8000/api";
+export const API_URL = "http://18.142.190.113:9002/api";
 
 //ALISDI ANG IP ADDRESS OG PORT KUNG KINSA NAG UP SA BACKEND SERVER
